7dc01e712d622fc598adf23fd872d84185eb76c6
alexchamorro0x <oriondigitalconsulting@gmail.com>
2022-12-13 00:22:48 +0900

    update blogs css

--- src/routes/blog/BlogOne.js
+++ src/routes/blog/BlogOne.js
@@ -464,7 +464,7 @@ export default function BlogOne() {
         <h2 className="text-3xl tracking-tight font-extrabold text-white sm:text-4xl">
           Orion Digital Consulting Blog
         </h2>
-        <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+        <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-400 sm:mt-4">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa libero
           labore natus atque, ducimus sed.
         </p>
@@ -482,7 +482,7 @@ export default function BlogOne() {
                 alt=""
               />
             </section>
-            <section className="flex-1 bg-gray-900 p-6 flex flex-col justify-between">
+            <section className="flex-1 bg-gray-800 p-6 flex flex-col justify-between">
               <section className="flex-1">
                 <Link
                   onClick={ResetLocation}
@@ -492,7 +492,7 @@ export default function BlogOne() {
                   <p className="text-xl font-semibold text-white">
                     {post.title}
                   </p>
-                  <p className="mt-3 text-base text-gray-500">
+                  <p className="mt-3 text-base text-gray-400">
                     {post.description}
                   </p>
                 </Link>
@@ -510,7 +510,7 @@ export default function BlogOne() {
                   <p className="text-sm font-medium text-white">
                     {post.author.name}
                   </p>
-                  <section className="flex space-x-1 text-sm text-gray-500">
+                  <section className="flex space-x-1 text-sm text-gray-400">
                     <time dateTime={post.datetime}>{post.date}</time>
                     <span aria-hidden="true">&middot;</span>
                     <span>{post.readingTime} read</span>
